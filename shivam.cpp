d8858915f5d8b4e4a4c5da49917e5fee83124c6e
Shivam <shivam16305@gmail.com>
2025-03-19 07:33:46 +0000

    minor changes

--- shivam.cpp
+++ shivam.cpp
@@ -1,7 +1,15 @@
-#include <iostream> 
+#include <iostream>
+
 using namespace std;
-int main() 
-{
- cout << "i am the best" << endl;
- return 0; 
+
+int main() {
+    double a, b;
+    
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    
+    cout << "Sum: " << (a + b) << std::endl;
+    
+    return 0;
 }
+
